66f7ee3051ef95e3fb8f3a49a5cd0d1e35f5729d
yingbinx <yingbinx.zeng@intel.com>
2019-03-19 14:08:26 +0800

    update

--- applypatch.sh
+++ applypatch.sh
@@ -44,7 +44,7 @@ function copy_src_files(){
         path=$1/$file
         dir=${path:${#SRC_DIR}}
         cp -rf $1/$file/. ${PRJ_HOME}/$dir
-        echo "  src copied     : ${dir}"
+        echo "    src copied   : ${dir}"
     done
 }
 
