701e646245d0a5bf85aee72e4c8b55d05730a73b
yingbinx <yingbinx.zeng@intel.com>
2019-03-21 13:41:35 +0800

    update

--- applypatch.sh
+++ applypatch.sh
@@ -8,7 +8,7 @@ SRC_DIR=${PATCH_HOME}/src
 
 function applypatch(){
     dir=$(dirname $1)
-    git_dir=${PRJ_HOME}/${dir:${#DIFF_DIR}}
+    git_dir=${PRJ_HOME}/${dir#*${DIFF_DIR}}
     cd $git_dir
     date_value=`grep "Date: " $1`
     patchexist=`git log --pretty=format:"Date: %aD" | grep "$date_value"`
@@ -17,19 +17,19 @@ function applypatch(){
     if [ -z "$patchexist" ] ; then
         git am $1 >& /dev/null
         if [ $? == 0 ]; then
-            echo "  patch appied   : ${file:${#DIFF_DIR}}"
+            echo "  patch applied   : ${file#*${DIFF_DIR}/}"
         else
-            echo "  conflict found : ${file:${#DIFF_DIR}}"
+            echo "  conflict found : ${file#*${DIFF_DIR}/}"
             git am --abort >& /dev/null
         fi
     fi
 }
 
-function scan_patch_files(){
+function apply_patch_files(){
     for file in `ls $1`
     do
         if [ -d $1"/"$file ] ; then
-            scan_patch_files $1"/"$file
+            apply_patch_files $1"/"$file
         else
             if [ ${file##*.} = "patch" ] ; then
                 applypatch $1"/"$file
@@ -41,16 +41,14 @@ function scan_patch_files(){
 function copy_src_files(){
     for file in `ls $1`
     do
-        path=$1/$file
-        dir=${path:${#SRC_DIR}}
-        cp -rf $1/$file/. ${PRJ_HOME}/$dir
-        echo "    src copied   : ${dir}"
+        cp -rf $1/$file ${PRJ_HOME}/
+        echo "     src copied   : ${file}"
     done
 }
 
 echo "Apply patches ..."
 
-scan_patch_files ${DIFF_DIR}
+apply_patch_files ${DIFF_DIR}
 copy_src_files ${SRC_DIR}
 
 echo "Apply patches done."
